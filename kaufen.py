479ac72a38437dc5373c2bcc117192db96be1b06
Carsten Haas <accs.ch05@gmail.com>
2026-08-28 11:11:05 +0200

    neue aufgabe

--- kaufen.py
+++ kaufen.py
@@ -1,11 +1,22 @@
 def kaufen(artikel, preis, anzahl):
-    return f"Ich kaufe {anzahl} {artikel} für insgesamt {preis * anzahl} Euro."
+    kosten = preis * anzahl
+    print(f"Ich kaufe {anzahl} {artikel} für insgesamt {kosten} Euro.")
+    return kosten
+
+
+def rueckgeldGeben(preis, gegeben):
+    rueckgeld = gegeben - preis
+    if rueckgeld < 0:
+        print(
+            f"Du hast nicht genug Geld gegeben. Du brauchst noch {abs(rueckgeld)} €.")
+    else:
+        print(f"Hier ist dein Rückgeld: {rueckgeld} €.")
 
 
 if __name__ == "__main__":
     artikel = "Käsebrot"
     preis = 2.5
     anzahl = 3
-
-    ausgabe = kaufen(artikel, preis, anzahl)
-    print(ausgabe)
+    gegeben = 3
+    kosten = kaufen(artikel, preis, anzahl)
+    rueckgeldGeben(kosten, gegeben)
