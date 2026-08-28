8be46869cef6db75600a69c8db9839a66ba0a880
SchmitzColin <schmitz.colin@gmx.de>
2026-08-28 10:26:54 +0200

    Abgabe kaufen.py

---
+++ Schultag.py
@@ -0,0 +1,13 @@
+# --- FUNKTIONSDEFENITION (Wartet nur, wird noch NICHT ausgeführt!) ---
+def gehe_zur_schule(snack):
+    print("Anziehen und losgehen...")
+    print(f"Auf dem Schulweg esse ich: {snack}")
+
+# --- EINSPRUNGPUNKT (Hier startet das Programm wirklich!) ---
+if __name__ == '__main__':
+    print("Schulmorgen beginnt.")
+
+    mein_brot = "Käsebrot"
+    gehe_zur_schule(mein_brot)
+
+    print("in der Klasse Hinsetzen")
---
+++ kaufen.py
@@ -0,0 +1,9 @@
+def kaufen(artikel, preis, anzahl):
+    kosten = preis * anzahl
+    print("Ich kaufe " + str(anzahl) + "x Schokolade für insgesamt " + str(kosten) + " Euro.")
+
+if __name__ == '__main__':
+    Artikelname = "Schokolade"
+    Preis = 1.5
+    Anzahl = 3
+    kaufen(Artikelname, Preis, Anzahl)
