4689b16b9e030eea0b331e1b57dad1f591f04bae
SchmitzColin <schmitz.colin@gmx.de>
2026-08-28 10:48:04 +0200

    Variablen klein & print leerzeichen hinzugefügt

--- kaufen.py
+++ kaufen.py
@@ -1,9 +1,9 @@
 def kaufen(artikel, preis, anzahl):
     kosten = preis * anzahl
-    print("Ich kaufe " + str(anzahl) + "x" + str(artikel) + "für insgesamt " + str(kosten) + " Euro.")
+    print("Ich kaufe " + str(anzahl) + "x " + str(artikel) + " für insgesamt " + str(kosten) + " Euro.")
 
 if __name__ == '__main__':
-    Artikelname = "Schokolade"
-    Preis = 1.5
-    Anzahl = 3
-    kaufen(Artikelname, Preis, Anzahl)
+    artikelname = "Schokolade"
+    preis = 1.5
+    anzahl = 3
+    kaufen(artikelname, preis, anzahl)
