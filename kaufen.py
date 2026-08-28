ee11f573d7258cce0481d76338df7f5295173935
FLNS1991 <laurinschubert@proton.me>
2026-08-28 11:07:01 +0200

    Aufgabe hinzugefuegt

--- kaufen.py
+++ kaufen.py
@@ -1,11 +1,10 @@
 def kaufen(artikel, preis, anzahl):
-    gesamtpreis = (preis * anzahl)
-    print("Ich kaufe 3x  für insgesamt", gesamtpreis,  "Euro.")
-    
-    
-    if __name__ == "__main__":
-        artikel = "Schokolade"
-        preis = 4.5
-        anzahl = 3
-        kaufen(artikel, preis, anzahl)
-     
+    gesamtpreis = preis * anzahl
+    print("Ich kaufe", anzahl, "x", artikel, "für insgesamt", gesamtpreis, "Euro.")
+
+
+if __name__ == "__main__":
+    artikel = "Schokolade"
+    preis = 4.5
+    anzahl = 3
+    kaufen(artikel, preis, anzahl)
