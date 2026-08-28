3318b2098649f4ef33a3e7d9c199ac860c6ea768
Silas Firat <silasfirat@gmail.com>
2026-08-28 10:39:55 +0200

    abgabe

---
+++ Kaufen.py
@@ -0,0 +1,9 @@
+
+def kaufen (artikel, preis, anzahl):
+    print(f"Ich Kaufe (anzahl) (artikel) für insgesamt (preis) Euro")
+    anzahl = 3
+    artikel = Schokolade
+    preis = 4.5
+
+if __name__== "__main__":
+    kaufen
