ed69f18baa797d386af0be098ab1bba6fe72e910
SchmitzColin <schmitz.colin@gmx.de>
2026-08-28 11:16:16 +0200

    ergänzung der rueckgeldGeben funktion unf mehrere reformatierungen

--- kaufen.py
+++ kaufen.py
@@ -1,9 +1,20 @@
 def kaufen(artikel, preis, anzahl):
     kosten = preis * anzahl
-    print("Ich kaufe " + str(anzahl) + "x " + str(artikel) + " für insgesamt " + str(kosten) + " Euro.")
+    return kosten
+
+def rueckgeldGeben(preis, gegeben):
+    rueckgeld = gegeben - preis
+    return rueckgeld
 
 if __name__ == '__main__':
+
+    # variablem zum Kauf
     artikelname = "Schokolade"
     preis = 1.5
     anzahl = 3
-    kaufen(artikelname, preis, anzahl)
+    gegeben = 10
+
+    # Ausfuehren der Methoden und Feedback via print
+    kosten = kaufen(artikelname, preis, anzahl)
+    rueckgeld = rueckgeldGeben(kosten, gegeben)
+    print(f"Ich kaufe {anzahl}x {artikelname} für insgesamt {kosten} Euro.\nEs wurden {gegeben} Euro bezahlt.\nDas ruekgeld betraegt: {rueckgeld}.")
