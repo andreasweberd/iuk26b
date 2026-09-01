3019b0fc648f125edc1e367c4793d3852e34cc36
Max <maxleogott@web.de>
2026-09-01 13:49:00 +0200

    Hinzufügen Rückgeld

--- Kaufen.py
+++ Kaufen.py
@@ -1,6 +1,20 @@
-def kaufen(artikel, preis, anzahl):
-    gesammt = preis * anzahl 
-    print(f"Ich habe {anzahl}x {artikel} gekauft für {gesammt}€")
+def Einkauf():
+
+    def kaufen(artikel, preis, anzahl):
+        global gesammt        
+        gesammt = (preis * anzahl)
+        print(f"Ich habe {anzahl}mal {artikel} gekauft für insgesammt {gesammt}€")
+
+
+    def bezahlen(einzahlung, gesammt):
+        rueckzahlung = (einzahlung - gesammt)
+        if rueckzahlung > 0:
+            print(f"Mein Rückgeld beträgt {rueckzahlung}€.")
+        else:
+            print(f"Ich wünsche Ihnen einen schönen Tag!")
+
+    kaufen("Schokolade", 5, 3)
+    bezahlen(20, gesammt)
 
 if __name__ == "__main__":
-    kaufen("Schokolade", 5, 3)
+    Einkauf()
