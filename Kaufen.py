b8216f90ab9016436e4b660d5859f8201e3a3f08
Max <maxleogott@web.de>
2026-09-03 12:52:56 +0200

    Optimierung

--- Kaufen.py
+++ Kaufen.py
@@ -1,20 +1,18 @@
+def kaufen(artikel, preis, anzahl):
+    global gesammt        
+    gesammt = (preis * anzahl)
+    print(f"Ich habe {anzahl}mal {artikel} gekauft für insgesammt {gesammt}€")
+    # return gesammt
+
+def bezahlen(einzahlung, gesammt):
+    rueckzahlung = einzahlung - gesammt
+    if rueckzahlung > 0:
+        print(f"Mein Rückgeld beträgt {rueckzahlung}€.")
+    else:
+        print(f"Ich wünsche Ihnen einen schönen Tag!")
+
 def Einkauf():
-
-    def kaufen(artikel, preis, anzahl):
-        global gesammt        
-        gesammt = (preis * anzahl)
-        print(f"Ich habe {anzahl}mal {artikel} gekauft für insgesammt {gesammt}€")
-
-
-    def bezahlen(einzahlung, gesammt):
-        rueckzahlung = (einzahlung - gesammt)
-        if rueckzahlung > 0:
-            print(f"Mein Rückgeld beträgt {rueckzahlung}€.")
-        else:
-            print(f"Ich wünsche Ihnen einen schönen Tag!")
-
-    kaufen("Schokolade", 5, 3)
-    bezahlen(20, gesammt)
+    bezahlen(20, kaufen("Schokolade", 5, 3))
 
 if __name__ == "__main__":
     Einkauf()
