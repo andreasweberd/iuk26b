fe118e2aa709127cf54079ec384eb8c0fc54bc85
Friedhelm Laurin Schubert <fschubert@it-haus.com>
2026-09-04 11:04:57 +0200

    Aufgabe 2

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
 def packe_rucksack(fach, snack):
-    print("\n", "Rucksack wird gepackt...","\n", "Buch für", fach, "eingesteckt", "\n", snack, "als Pausebrot eingepackt.")
+    print("\n", "Rucksack wird gepackt...","\n", "Buch für", fach, "eingesteckt.", "\n", snack, "als Pausebrot eingepackt.")
+    # print(f"Rucksack wird gepackt...\n"+
+    #       f"-> Buch für {fach} eingesteckt.\n"+
+    #       f"-> {snack} als Pausebrot eingepackt.")
 
 def gehe_weg(strecke_km, geschwindigkeit_kmh):
     dauer = (strecke_km / geschwindigkeit_kmh) * 60
-    print("Schulweg wird angetreten...", "\n", "Für ", strecke_km, "benötigst du ", dauer, " Minuten.")
+    print("\n", "Schulweg wird angetreten...", "\n", "Für ", strecke_km, "benötigst du ", dauer, " Minuten.")
 
 if __name__ == '__main__':
     packe_rucksack('LF5', 'Apfel')
