2eebf009723cca55bcfc20fc8142c7189205ac71
SchmitzColin <schmitz.colin@gmx.de>
2026-09-04 10:29:26 +0200

    Aufgabe2 gelöst

---
+++ 040926/Morgenrutine.py
@@ -0,0 +1,16 @@
+def packe_rucksack(fach, snack):
+    print(f"\nRucksack wird gepackt...\n-> Buch wird für {fach} eingepackt,\n-> {snack} als Pausenbrot eingepackt")
+
+def gehe_weg(strecke_km, geschwindigkeit_kmh):
+    dauer = strecke_km / geschwindigkeit_kmh * 60
+    print(f"\nSchulweg wird angetreten...\n-> Für {strecke_km} km benötigst du {dauer} Minuten")
+
+if "__main__" == __name__:
+    fach = input("Fach angeben: ")
+    snack = input("Snack angeben: ")
+    strecke_km = 3.5
+    geschwindigkeit_kmh = 4.0
+
+
+    packe_rucksack(fach, snack)
+    gehe_weg(strecke_km, geschwindigkeit_kmh)
