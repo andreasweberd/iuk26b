95e64dfb200da72839b7da5ee78ecc8dddc2414e
Carsten Haas <accs.ch05@gmail.com>
2026-09-04 10:20:20 +0200

    V1

---
+++ aufgabe2.py
@@ -0,0 +1,18 @@
+def packe_rucksack(fach, snack):
+    print(f'🎒 Rucksack wird gepackt...')
+    print(f'-> Buch für {fach} eingesteckt.')
+    print(f'-> {snack} als Pausenbrot eingepackt.')
+
+
+def gehe_weg(strecke_km, geschwindigkeit_kmh):
+    dauer = (strecke_km / geschwindigkeit_kmh) * 60
+    print(f'🚶 Schulweg wird angetreten...')
+    print(f'-> Für {strecke_km} km benötigst du {dauer} Minuten.')
+
+
+if __name__ == "__main__":
+    fach = "LF01"
+    snack = "Käsebrot"
+
+    packe_rucksack(fach, snack)
+    gehe_weg(2.5, 4)
