e730df11813610f6d6f16272d33d46575322eb34
Justin <117776000+StelarRug8048@users.noreply.github.com>
2026-09-04 16:43:44 +0200

    Abgabe Unterricht

---
+++ Schultag.py
@@ -0,0 +1,10 @@
+def gehe_zur_Schule(snack):
+    print("Ich gehe zur Schule.")
+    print("Ich habe einen Snack dabei:", snack)
+
+if __name__ == "__main__":
+    print("Schultag beginnt.")
+    mein_brot = "Käsebrot"
+    gehe_zur_Schule(mein_brot)
+
+    print("In der Klasse hinsetzen.")
