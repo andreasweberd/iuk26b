e9f369aa5ee6ff43a0890c1bf5058f31883fe3ce
Friedhelm Laurin Schubert <fschubert@it-haus.com>
2026-09-04 10:50:51 +0200

    Aufgabe 2

---
+++ main.py
@@ -0,0 +1,6 @@
+def testFunktion(name):
+    print(f'Hi, {name}')
+    
+# Einsprungspunkt
+if __name__ == '__main__':
+    testFunktion('Laurin')
---
+++ script.py
@@ -0,0 +1,10 @@
+def packe_rucksack(fach, snack):
+    print("\n", "Rucksack wird gepackt...","\n", "Buch für", fach, "eingesteckt", "\n", snack, "als Pausebrot eingepackt.")
+
+def gehe_weg(strecke_km, geschwindigkeit_kmh):
+    dauer = (strecke_km / geschwindigkeit_kmh) * 60
+    print("Schulweg wird angetreten...", "\n", "Für ", strecke_km, "benötigst du ", dauer, " Minuten.")
+
+if __name__ == '__main__':
+    packe_rucksack('LF5', 'Apfel')
+    gehe_weg(2.5, 5)
