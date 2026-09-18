dc61514b19f846d4fdde3aee3e51d2c4bfbf05c0
Friedhelm Laurin Schubert <fschubert@it-haus.com>
2026-09-18 11:06:27 +0200

    Aufgabe1.py

--- 180926/Aufgabe1.py
+++ 180926/Aufgabe1.py
@@ -6,7 +6,18 @@ def berechne_bild(breite, hoehe, farbtiefe):
     
     return mebibytes
 
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
+    audio_bits = abtastrate * bittiefe * kanaele * zeit_in_sekunden
+    audio_bytes = audio_bits / 8
+    audio_kibibytes = audio_bytes / 1024
+    audio_mebibytes = audio_kibibytes / 1024
+    
+    return audio_mebibytes
+
 if __name__ == "__main__":
     ergebnis = berechne_bild(1025, 680, 16)
-    print(f"Die Datei ist {ergebnis} MiB groß.")
+    audio_ergebnis = berechne_audio(44100, 16, 2, 10)
+    print(f"Die Datei ist {ergebnis} MiB und die Audiodatei ist {audio_ergebnis} MiB groß.")
+
+    
     
