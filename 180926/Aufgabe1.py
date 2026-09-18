4868b849fc80c93fc21653e73755a5156e26cff8
Max <maxleogott@web.de>
2026-09-18 11:13:07 +0200

    Ich ahbe wieder super cool commited

--- 180926/Aufgabe1.py
+++ 180926/Aufgabe1.py
@@ -2,13 +2,35 @@ def berechne_bild(breite, hoehe, farbtiefe):
     Bits = breite * hoehe * farbtiefe
     Bytes = Bits / 8
     Kibibytes = Bytes / 1024
-    # Kibibits = Bits / 1024
-    # Kibibytes = Kibibits / 8
     Mebibytes = Kibibytes / 1024
-    print(Bits)
-    # print(Kibibits)
-    print(Kibibytes)
-    print(Mebibytes)
+    return Mebibytes
+
+def berechne_audio(abtastrate, bittiefe, kanaele, sekunden):
+    Audio_Bits = abtastrate * bittiefe * kanaele * sekunden
+    Audio_Bytes = Audio_Bits / 8
+    Audio_Kibibytes = Audio_Bytes / 1024
+    Audio_Mebibytes = Audio_Kibibytes / 1024
+    return Audio_Mebibytes
 
 if __name__ == "__main__":
-    berechne_bild(1025, 680, 16)
+    ergebnis = berechne_bild(1025, 680, 16)
+    audio_ergebnis = berechne_audio(44100, 16, 2, 10)
+    print(f"Die Bilddatei ist {ergebnis} Mebibytes groß und die Audiodatei ist {audio_ergebnis} Mebibytes groß")
+
+# def berechne_bild(breite, hoehe, farbtiefe):
+#     Bits = breite * hoehe * farbtiefe
+#     bitsUmrechner(Bits)
+
+# def berechne_audio(abtastrate, bittiefe, kanaele, sekunden):
+#     Bits = abtastrate * bittiefe * kanaele * sekunden
+#     bitsUmrechner(Bits)
+
+# def bitsUmrechner(Bits):
+#     Bytes = Bits / 8
+#     Kibibytes = Bytes / 1024
+#     Mebibytes = Kibibytes / 1024
+#     print(f"Die ursprünglichen {Bits} Bits sind umgerechnet {Mebibytes}")
+
+# if __name__ == "__main__":
+#     berechne_bild(1025, 680, 16)
+#     berechne_audio(44100, 16, 2, 10)
