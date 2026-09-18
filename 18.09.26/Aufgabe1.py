0e6a8565807dd177c122545b3a6d53762189082e
ginabaer <ginabaer0512@googlemail.com>
2026-09-18 11:12:47 +0200

    Ich habe die Aufgabe gelöst

--- 18.09.26/Aufgabe1.py
+++ 18.09.26/Aufgabe1.py
@@ -5,7 +5,15 @@ def berechne_bild(breite, hoehe, farbtiefe):
     mebibytes = kibibytes / 1024
     return mebibytes
 
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
+    audio_bits = abtastrate * bittiefe * kanaele * zeit_in_sekunden
+    audio_anzahl_bytes = audio_bits / 8
+    audio_kibibytes = audio_anzahl_bytes / 1024
+    audio_mebibytes = audio_kibibytes / 1024
+    return audio_mebibytes
+
 
 if __name__ == "__main__":
     ergebnis = berechne_bild(1025, 680, 16)
-    print(f"Die Dateigröße ist {ergebnis:.2}")
+    audio_ergebnis = berechne_audio(44100, 16, 2, 10)
+    print(f"Die Dateigröße ist {ergebnis:.2} MiB und die Audiodateigröße ist {audio_ergebnis:.2} MiB groß.")
