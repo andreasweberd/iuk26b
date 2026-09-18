9b56b08cc1ce98507362d0fc5a0e40fc6df0849c
Fabio Eschle <eschle.fabio@web.de>
2026-09-18 11:07:39 +0200

    Abgabe der Überarbeiteten Aufgabe

--- 18092026/Aufgabe1.py
+++ 18092026/Aufgabe1.py
@@ -5,6 +5,15 @@ def berechne_bild(breite, hoehe, farbtiefe):
     mebibytes = kibibytes / 1024
     return mebibytes
 
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sek):
+    Audio_Bits = abtastrate * bittiefe * kanaele * zeit_in_sek
+    bytes = Audio_Bits / 8
+    kibibytes = bytes / 1024
+    mebibytes = kibibytes / 1024
+    return mebibytes
+
 if __name__ == '__main__':
     Bild = berechne_bild(1025, 680, 16)
-    print(f"Das bild hat eine größe von {Bild} Mebibyte")
+    Audio = berechne_audio(44100, 16, 2, 10)
+    print(f"Die Bilddatei hat eine größe von {Bild} Mebibyte")
+    print(f"Die Audiodatei hat eine größe von {Audio} Mebibyte")
