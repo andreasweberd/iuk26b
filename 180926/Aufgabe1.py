513ace8f04c1b5880712dbabb948bef86eba7d20
Silas Firat <silasfirat@gmail.com>
2026-09-18 11:13:15 +0200

    Mit Audiodateigroeße Berechnung

--- 180926/Aufgabe1.py
+++ 180926/Aufgabe1.py
@@ -4,6 +4,15 @@ def berechne_bild(breite, hoehe, farbtiefe):
     kibibytes = bytes / 1024
     mebibytes = kibibytes / 1024
     return mebibytes
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
+    audio_bits = abtastrate * bittiefe * kanaele * zeit_in_sekunden
+    audio_bytes = audio_bits / 8
+    kibibytes = audio_bytes / 1024
+    mebibytes = kibibytes / 1024
+    return mebibytes
 if __name__ == "__main__":
-    ergebnis = berechne_bild(1025,680,16)
-    print(f"Die Dateigroeße ist {ergebnis}")
+    ergebnis_bild = berechne_bild(1025,680,16)
+    print(f"Die Dateigroeße ist {ergebnis_bild} Mebibytes")
+    ergebnis_audio = berechne_audio(44100,16,2,10)
+    print(f"Die Audiodateigroeße ist {ergebnis_audio} Mebibytes")
+
