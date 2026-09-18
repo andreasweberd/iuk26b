3c05f9512d445e0d0dd437589f53a1f6b149de01
steuernagel_moshe <mosteuernagel2@gmail.com>
2026-09-18 11:12:16 +0200

    Aufgabe 1 mit audio

--- 180926/Aufgabe1.py
+++ 180926/Aufgabe1.py
@@ -6,9 +6,18 @@ def berechne_bild(breite,hoehe,tiefe):
     mebibytes = kibibytes / 1024
     return mebibytes
 
+def berechne_audio(abtastrate,bittiefe,kanaele,zeit_in_sekunden):
+    audio_bits = abtastrate * bittiefe * kanaele * zeit_in_sekunden
+    audio_bytes = audio_bits / 8
+    audio_kibibytes = audio_bytes / 1024
+    audio_mebibytes = audio_kibibytes / 1024
 
+    return audio_mebibytes
 
 if __name__ == "__main__":
 
     ergebnis = berechne_bild(1025,680,16)
     print(f" Die Dateigröße ist {ergebnis}")
+
+    Audioergebnis = berechne_audio(44100,16,2,10)
+    print(f"Die Audiogröße ist {Audioergebnis}")
