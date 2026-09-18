a47d699f6cb39f59cf47402fb9197b54e0f9e6ac
aaron <britzaaron@gmail.com>
2026-09-18 11:06:26 +0200

    Lösung Aufgabe 2

--- 18092026/Aufgabe1.py
+++ 18092026/Aufgabe1.py
@@ -5,9 +5,15 @@ def main():
             # Die Größe wird somit direkt in MiB berechnet
             return (breite * hoehe * farbtiefe)  / 8388608
 
+    def berechne_audio(abtastrate, bittiefe:int, kanaele, zeit):
+        # Die 8.388.608 entsteht durch die Multiplikation von 8 und 1024^2
+        # Die Größe wird somit direkt in MiB berechnet
+        return (abtastrate*bittiefe*kanaele*zeit) / 8388608
+
+    # Bild
     print (f"Der benötigte Speicher für dieses Bild beträgt {berechne_bildbreite(1025,680,16)}MiB")
-
-
+    # Audio
+    print (f"Der benötigte Speicher für diese Audio beträgt {berechne_audio(44100,16,2,10)}MiB")
 
 if __name__ == "__main__":
     main()
