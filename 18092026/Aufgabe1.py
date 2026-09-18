a624c3855d381fdb9ff3eaa656dfafffa1404002
Fabio Eschle <eschle.fabio@web.de>
2026-09-18 10:41:01 +0200

    Ich habe die Aufgabe 1 gelöst

---
+++ 18092026/Aufgabe1.py
@@ -0,0 +1,10 @@
+def berechne_bild(breite, hoehe, farbtiefe):
+    bits = breite * hoehe * farbtiefe
+    bytes = bits / 8
+    kibibytes = bytes / 1024
+    mebibytes = kibibytes / 1024
+    return mebibytes
+
+if __name__ == '__main__':
+    Bild = berechne_bild(1025, 680, 16)
+    print(f"Das bild hat eine größe von {Bild} Mebibyte")
