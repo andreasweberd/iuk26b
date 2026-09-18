29fdabd908036f23611dcd3f73ccafdf63eb15e2
SchmitzColin <schmitz.colin@gmx.de>
2026-09-18 11:05:59 +0200

    Abgabe von Aufgabe 2

--- 18.09.26/Aufgabe1.py
+++ 18.09.26/Aufgabe1.py
@@ -1,9 +1,15 @@
 def berechne_bild(breite, hoehe, farbtiefe):
-    groesse = breite * hoehe * farbtiefe # Bits
-    groesse /= 8 # Bytes
-    groesse /= 1024 ** 2 # Mebibytes
+    groesse = breite * hoehe * farbtiefe    # Bits
+    groesse /= 8                            # Bytes
+    groesse /= 1024 ** 2                    # Mebibytes
+    return groesse
+
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit):
+    groesse = abtastrate * bittiefe * kanaele * zeit    # bits
+    groesse /= 8 * (1024 ** 2)                          # Mebibytes
     return groesse
 
 if __name__ == "__main__":
     bild_groesse_mebibytes = round(berechne_bild(1025, 680, 16), 2)
-    print(f"Das Bild nimmt {bild_groesse_mebibytes} MiBs Speicher ein.")
+    audio_groesse_mebibytes = round(berechne_audio(44100, 16, 2, 10), 2)
+    print(f"Das Bilddatei nimmt {bild_groesse_mebibytes} MiBs Speicherplatz ein.\nDie Audiodatei nimmt {audio_groesse_mebibytes} MiBs Speicherplatz ein.")
