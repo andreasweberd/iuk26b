839ff174cf0ea3db4e8b004ab8e6383c57080bfa
cedric thomm <cedric@cedrico.de>
2026-09-18 10:45:49 +0000

    ich habe die aufgabe gelöst

---
+++ 180926/Aufgabe1.py
@@ -0,0 +1,15 @@
+def berechne_bild(breite, hoehe, farbtiefe):
+    bits = breite * hoehe * farbtiefe
+    anzahl_bytes = bits / 8
+    kibibytes = anzahl_bytes / 1024
+    mebibytes = kibibytes / 1024
+    return mebibytes
+
+
+if __name__ == "__main__":
+    ergebnis = berechne_bild(1025, 680, 16)
+    print(f"Die Dateigroesse ist {ergebnis:.2f} Mebibyte.")
+
+
+
+
