a328158b0c965e2a3df37310da638807ee852069
David Odenbreit <dodenbreit@it-haus.com>
2026-09-21 07:29:11 +0200

    Lösung ufgabe 1

--- 180926/aufgabe1.py
+++ 180926/aufgabe1.py
@@ -3,17 +3,16 @@ def berechne_bild(breite, hoehe, farbtiefe):
         groesse_mib = groesse_bits / 8 / 1024 / 1024
         print(f"Das Bild hat eine Größe von {groesse_mib} MiB.")
 
- 
-berechne_bild(1025, 680, 16)
+  
 
 def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
-        groesse = float(abtastrate, bittiefe, kanaele, zeit_in_sekunden)
+        groesse = float(abtastrate * bittiefe * kanaele * zeit_in_sekunden)
         groesse_m = groesse / 8 / 1024 / 1024 
         print(f"Die Datei ist { groesse_m} mib groß")
 
-berechne_audio(44100, 16, 2, 10)
+
  
 if __name__ == "__main__":
-        berechne_bild(1025, 680, 16)
+        berechne_bild(1025, 680, 16) 
         berechne_audio(44100, 16, 2, 10)
  
