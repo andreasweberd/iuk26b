712a14b159ab884c6d7c0207d4abf6df05fc7720
Timon Lutz <t.lutz@rz.hochschule-trier.de>
2026-09-23 15:13:48 +0200

    Lösung vom 18.09.26

--- 180926/Augabe 18.09.26.py
+++ 180926/Augabe 18.09.26.py
@@ -1,12 +1,26 @@
 
 def berechne_bild(breite, höhe,farbtiefe):
-bits = breite * hoehe * farbtiefe
-bytes = bits / 8
-kibibytes = bytes / 1024
-mibibytes = kibibytes / 1024
+    bits = breite * höhe * farbtiefe
+    bytes = bits / 8
+    kibibytes = bytes / 1024
+    mibibytes = kibibytes / 1024
 
-return bits
+    return mibibytes
 
-if__name__ == "__main__":
-ergebins = berechne_bild(1025, 680, 116)
-print(f"Die dateigröße ist {ergebins}")
+
+def berechne_audio(abtastrate, bittiefe, kanaele, zeit_in_sekunden):
+    #Audiobit-Bits berechnen: abstrate * bittiefe * kanaele * zeit
+    audio_bits= abtastrate * bittiefe * kanaele * zeit_in_sekunden
+    audio_bytes   = audio_bits / 8
+    audio_kibibytes = audio_bytes / 1024
+    audio_mebibytes = audio_kibibytes / 1024
+
+    return audio_mebibytes
+
+if __name__ == "__main__":
+    ergebins = berechne_bild(1025, 680, 16)
+    print(f"Die Bild Datei ist {ergebins} groß")
+    ergebins = berechne_audio(44100, 16, 2, 10)
+    print(f"Die Audio Datei ist {ergebins} groß")
+
+
---
+++ Aufhabe2.py
@@ -0,0 +1,53 @@
+
+
+def packe_rucksack (fach, snack):
+
+    print (f"Buch für {fach} eingesteckt.")
+    print (f"{snack} als Pausebrot eingepackt")
+
+def gehe_weg (strecke_km, geschwindigkeit_kmh):
+    dauer = (strecke_km / geschwindigkeit_kmh) * 60
+    print ("Schulweg wird angetreten...")
+    print (f"{dauer}")
+if __name__ == "__main__":
+
+if __name__ == "__main__":
+    packe_rucksack("fach", "snack")
+    gehe_weg(10, 6)
+
+
+
+
+
+# # 1. Erstelle die erste Funktion: packe_rucksack
+
+#    # Die Funktion benötigt zwei Parameter: fach (Text) und snack (Text).
+
+#     Sie soll folgenden Text auf der Konsole ausgeben:
+#     Plaintext
+
+#     🎒 Rucksack wird gepackt...
+#      -> Buch für LF5 eingesteckt.
+#      -> Apfel als Pausebrot eingepackt.
+
+# 2. Erstelle die zweite Funktion: gehe_weg
+
+#     Die Funktion benötigt zwei Parameter: strecke_km (Kommazahl) und geschwindigkeit_kmh (Kommazahl).
+
+#     Berechne in der Funktion die Dauer in Minuten mit der Formel:
+
+#     dauer = (strecke_km / geschwindigkeit_kmh) * 60
+
+#     Sie soll folgenden Text auf der Konsole ausgeben:
+#     Plaintext
+
+#     🚶 Schulweg wird angetreten...
+#      -> Für 2.5 km benötigst du 30.0 Minuten.
+
+# 3. Schreibe das Hauptprogramm (Einsprungpunkt)
+
+#     Nutze den Einsprungpunkt if __name__ == "__main__":.
+
+#     Lege dort passende Variablen für dein Fach, deinen Snack, die Strecke und deine Geschwindigkeit an.
+
+#     Rufe erst packe_rucksack(...) und danach gehe_weg(...) mit deinen Variablen auf.
