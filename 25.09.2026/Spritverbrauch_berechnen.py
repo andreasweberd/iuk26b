124bd584f3a93bf6adb88a212ba5c64966faa22a
SchmitzColin <schmitz.colin@gmx.de>
2026-09-25 08:41:45 +0200

    Typecast von int auf float geaendert

--- 25.09.2026/Spritverbrauch_berechnen.py
+++ 25.09.2026/Spritverbrauch_berechnen.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
-    gefahrene_km = int(input("Gib die gefahrenen km an: "))
-    liter = int(input("Gib die verbrauchten Liter an: "))
+    gefahrene_km = float(input("Gib die gefahrenen km an (z.B.: 148.4): "))
+    liter = float(input("Gib die verbrauchten Liter an (z.B.: 10.6): "))
     verbrauch = round(liter / gefahrene_km * 100, 2)
     print(f"Der Verbrauch ist {verbrauch}L/100km -> " + ("hoch." if (verbrauch > 8) else "sparsam."))
     # ich weiß ich haette einfach ne normale if bedingung machen können aber mir war langeweilig also hab ich es komplizierter wie nötig gemacht :D
