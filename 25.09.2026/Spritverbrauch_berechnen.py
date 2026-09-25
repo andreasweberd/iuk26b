005e9e3b51125099e0ff95f7e5b196bf7979de61
SchmitzColin <schmitz.colin@gmx.de>
2026-09-25 09:01:50 +0200

    Auslagerung der Verbrauchsberechnung in eine Funktion

--- 25.09.2026/Spritverbrauch_berechnen.py
+++ 25.09.2026/Spritverbrauch_berechnen.py
@@ -1,6 +1,9 @@
+def berechne_verbrauch(strecke_km, liter):
+    return round(liter / gefahrene_km * 100, 2)
+
 if __name__ == "__main__":
     gefahrene_km = float(input("Gib die gefahrenen km an (z.B.: 148.4): "))
     liter = float(input("Gib die verbrauchten Liter an (z.B.: 10.6): "))
-    verbrauch = round(liter / gefahrene_km * 100, 2)
+    verbrauch = berechne_verbrauch(gefahrene_km, liter)
     print(f"Der Verbrauch ist {verbrauch}L/100km -> " + ("hoch." if (verbrauch > 8) else "sparsam."))
     # ich weiß ich haette einfach ne normale if bedingung machen können aber mir war langeweilig also hab ich es komplizierter wie nötig gemacht :D
