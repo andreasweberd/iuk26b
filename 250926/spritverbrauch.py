7ddbfd1459f6bf84aa2706bb08f9b44318e548f0
Carsten Haas <accs.ch05@gmail.com>
2026-09-25 09:19:27 +0200

    upgrade preis

--- 250926/spritverbrauch.py
+++ 250926/spritverbrauch.py
@@ -2,12 +2,20 @@ def berechne_spritverbrauch(getankte_liter, gefahrene_kilometer):
     return (getankte_liter / gefahrene_kilometer) * 100
 
 
+def berechne_gesamtkosten(getankte_liter, aktueller_spritpreis):
+    return getankte_liter * aktueller_spritpreis
+
+
 def main():
     getankte_liter = float(input("getankte Liter eingeben: "))
     gefahrene_kilometer = float(input("gefahrene Kilometer eingeben: "))
+    aktueller_spritpreis = float(input("GEBE AKTUELLEN SPRITPREIS EIN!!!!!: "))
 
     verbrauch = berechne_spritverbrauch(getankte_liter, gefahrene_kilometer)
+    gesamtkosten = berechne_gesamtkosten(getankte_liter, aktueller_spritpreis)
+
     print(f"Der Verbrauch ist {verbrauch} l/100 km")
+    print(f"Es kostet {gesamtkosten}€")
 
     if (verbrauch > 8):
         print("Du saugst richtig viel du penner")
